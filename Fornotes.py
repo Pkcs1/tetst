0a67572a8fbbe21745684ef078dae64cbf8fe99a
Pkcs1 <ramaoctantyo@gmail.com>
2026-06-22 21:08:01 +0700

    small note

--- Fornotes.py
+++ Fornotes.py
@@ -1,3 +1,4 @@
+
 shikai = ["Zangetsu", "Hyorinmaru", "Senbonzakura", "Sakanade", "Kyokasuigetsu"]
 """
 index = 0
@@ -8,3 +9,9 @@ for name in shikai:
 #Or use enumerate(), also can have an optional start parameter
 for index, name in enumerate(shikai):
     print(f"Index {index} dari shikai {name}")
+
+#Iterate two lists with zip()
+Captains = ["Yamamoto", "Kenpachi", "Mayuri"]
+Ids = [1, 11, 12]
+for name, id in zip(Captains, Ids):
+    print(f"Name: {name}\nId: {id}")
---
+++ Listcomprehension.py
@@ -0,0 +1,6 @@
+odd_numbers = []
+
+for i in range(21):
+    if i % 2 != 0:
+        odd_numbers.append(i)
+print(odd_numbers)
